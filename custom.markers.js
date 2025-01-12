46fbc8ea224e8d9ddaa508fb34d5ca7b99b3ee20
Brune <168581255+BruneCoding@users.noreply.github.com>
2025-01-12 18:36:04 +0000

    Simple Markers

--- custom.markers.js
+++ custom.markers.js
@@ -38,27 +38,31 @@ Use the imageScale property if the pin image is too large.
 
 UnminedCustomMarkers = {
 
-    isEnabled: false,
+    isEnabled: true,
 
     markers: [
-
-        // Example 1: Simple marker
-        {
-            x: -200,
-            z: -200,
+            {
+            x: 3829,
+            z: 4702,
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
             imageScale: 0.5,
+            text: "Belgrade",
+            textColor: "red", 
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 20px Calibri,sans serif",
         },
+
 
         // Example 2: Marker with text
         {
-            x: 0,
-            z: 0,
+            x: 2943,
+            z: 5193,
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
             imageScale: 0.5,
-            text: "Marker with text",
+            text: "Nova Roma",
             textColor: "red", 
             offsetX: 0,
             offsetY: 20,
@@ -67,9 +71,9 @@ UnminedCustomMarkers = {
 
         // Example 3: Text only
         {
-            x: 200,
-            z: 200,
-            text: "Text only", 
+            x: 2173,
+            z: 4449,
+            text: "Spawn", 
             textColor: "yellow", 
             offsetX: 0,
             offsetY: 0, 
